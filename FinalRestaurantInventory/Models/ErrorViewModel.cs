611c27adfe8c5ab1c35f8ef511a84ff88a084552
David King <david.king550wil@gmail.com>
2022-08-03 20:36:48 -0400

    firstcommit

---
+++ FinalRestaurantInventory/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinalRestaurantInventory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
